077379d6329d00e50fab6d2e211da948298f3a6a
pchmielecki <pchmielecki87@gmail.com>
2020-05-30 10:10:08 +0200

    different way to install k8s

---
+++ DevOps/DevOps on K8S/sonarqube-k8s.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+#sudo sh -x <script_name>
+
+#update and upgrade
+sudo apt update -y && sudo apt upgrade -y
+
--- DevOps/uu-docker-kubernetes.sh
+++ DevOps/uu-docker-kubernetes.sh
@@ -3,6 +3,8 @@
 #working on ubu16+ (for Ubu 16 convert apt to apt-get)
 #based on docker docs > https://docs.docker.com/engine/install/ubuntu/
 #based on kubernetes docs > https://kubernetes.io/docs/tasks/tools/install-kubectl/ 
+#based on https://www.youtube.com/watch?v=MyNnVurtSf0
+#based on https://vitux.com/install-and-deploy-kubernetes-on-ubuntu/
 #based on microk8s on ubuntu docs > https://ubuntu.com/kubernetes/install/
 
 #update and upgrade
@@ -31,23 +33,61 @@ sudo apt update -y && sudo apt upgrade -y
     sudo apt install docker-ce docker-ce-cli containerd.io -y
 
     #enable docker to launch at boot
+    sudo systemctl start docker
     sudo systemctl enable docker
 
 ##kubernetes 
 
+    #curl
+    sudo apt install curl -y
+
+    #add key and install
+    curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
+
+cat <<EOF | sudo tee /etc/apt/sources.list.d/kubernetes.list
+deb https://apt.kubernetes.io/ kubernetes-xenial main
+EOF
+
+    sudo apt-get update -y
+    sudo apt-get install -y kubelet kubeadm kubectl
+    sudo apt-mark hold kubelet kubeadm kubectl
+
+    #restart kubenetes daemon
+    systemctl daemon-reload
+    systemctl restart kubelet
+
+    #disable swap memory (if running) on both the nodes
+    sudo swapoff -a
+
+    #set unique hostnames on each node
+    sudo hostnamectl set-hostname master-node
+    sudo hostnamectl set-hostname slave-node
+
+    #init
+    sudo kubeadm init
+    mkdir -p $HOME/.kube
+    sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
+    sudo chown $(id -u):$(id -g) $HOME/.kube/config
+
+    #deploy pod network through the master node
+    sudo kubectl apply -f https://raw.githubusercontent.com/coreos/flannel/master/Documentation/kube-flannel.yml
+
+#########################################################################################################################
+#commented because is not workind due to env var and kube.config issues
+
     #(optional) install snap store
-    sudo apt install snapd -y
+#    sudo apt install snapd -y
 
     #install components
-    snap install kubectl --classic              #Command line client for controlling a Kubernetes cluster
-    sudo snap install kubelet --classic         #Kubernetes “node agent” that runs on each node in Kubernetes
-    sudo snap install kube-proxy --classic      #Kubernetes network proxy that runs on each node
-    sudo snap install kube-apiserver            #Kubernetes master component that exposes the Kubernetes API
-    sudo snap install kube-controller-manager   #Kubernetes master component that runs controllers
-    sudo snap install kube-scheduler            #Kubernetes master component that assigns each newly created pod to a node
-    sudo snap install kubeadm --classic         #Tool for bootstrapping Kubernetes clusters
-    sudo snap install kubefed --classic         #Kubernetes cluster federation manager
-    sudo snap install helm --classic            #Kubernetes package manager
+#    snap install kubectl --classic              #Command line client for controlling a Kubernetes cluster
+#    sudo snap install kubelet --classic         #Kubernetes “node agent” that runs on each node in Kubernetes
+#    sudo snap install kube-proxy --classic      #Kubernetes network proxy that runs on each node
+#    sudo snap install kube-apiserver            #Kubernetes master component that exposes the Kubernetes API
+#    sudo snap install kube-controller-manager   #Kubernetes master component that runs controllers
+#    sudo snap install kube-scheduler            #Kubernetes master component that assigns each newly created pod to a node
+#    sudo snap install kubeadm --classic         #Tool for bootstrapping Kubernetes clusters
+#    sudo snap install kubefed --classic         #Kubernetes cluster federation manager
+#    sudo snap install helm --classic            #Kubernetes package manager
 
 ##(optional) microk8s
 
