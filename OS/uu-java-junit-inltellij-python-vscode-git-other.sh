a4f5746f5d1b9a0a01c45955b564d0c8491203fa
pchmielecki <pchmielecki87@gmail.com>
2020-06-11 12:23:40 +0200

    added chrome+driver

--- OS/uu-java-junit-inltellij-python-vscode-git-other.sh
+++ OS/uu-java-junit-inltellij-python-vscode-git-other.sh
@@ -48,6 +48,18 @@ sudo snap install --classic code # or code-insiders
 sudo apt install -y git
 sudo snap install gitkraken --classic
 
+#chrome chromedriver
+sudo curl -sS -o - https://dl-ssl.google.com/linux/linux_signing_key.pub | apt-key add
+sudo echo "deb [arch=amd64]  http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google-chrome.list
+sudo apt-get -y update
+sudo apt-get -y install google-chrome-stable
+wget https://chromedriver.storage.googleapis.com/2.41/chromedriver_linux64.zip
+unzip chromedriver_linux64.zip
+sudo mv chromedriver /usr/bin/chromedriver
+sudo chown root:root /usr/bin/chromedriver
+sudo chmod +x /usr/bin/chromedriver
+
+
 #other cool stuff
 sudo snap install spotify
 sudo snap install teams-for-linux
